0fb5fe01a9581278c08cd1e843eea30302f46023
HugoDemont <hugodemont62@hotmail.fr>
2024-12-19 15:47:55 +0100

    feat: update intro

--- src/scripts/component/intro.js
+++ src/scripts/component/intro.js
@@ -5,39 +5,27 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default class Intro {
 	constructor() {
-		// Récupérer les listes ou éléments ciblés et initialiser GSAP
 		this._getElements();
 		this._init();
 	}
 
 	_getElements() {
 		this.textRows = document.querySelectorAll('.home-intro__text__row');
-		this.images = document.querySelectorAll('.home-intro__text__row .image img');
+		this.images = document.querySelectorAll('.home-intro__text__row .image');
 	}
 
 	_init() {
-		gsap.from(this.textRows, {
-			opacity: 0,
-			y: 20,
+		gsap.from(this.images, {
+			scaleX: 0,
+			width: 0,
 			duration: 1,
 			stagger: 0.3,
 			ease: 'power2.out',
-			delay: 1,
-		});
-
-		gsap.from(this.images, {
-			opacity: 0,
-			x: -100,
-			scale: 0.5,
-			duration: 1,
-			stagger: 0.3,
-			ease: 'elastic.out(1, 0.3)',
-			delay: 1.5,
 			scrollTrigger: {
 				trigger: this.images,
 				start: 'top 80%',
 				end: 'bottom 10%',
-				toggleActions: 'play none none reverse',
+				scrub: true,
 			},
 		});
 	}
